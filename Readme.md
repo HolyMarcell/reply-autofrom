6f740ad5520ecac02f389d5554fe36a2346b308a
Tristan Döhl <tristan.doehl@holy-mail.de>
2021-06-09 12:38:31 +0200

    wip

---
+++ Readme.md
@@ -0,0 +1,6 @@
+## Reply-Autofrom
+
+A Thunderbird addon that automatically repies from the emailaddress that your email has
+originally been sent to.
+
+
---
+++ src/background.js
@@ -0,0 +1,16 @@
+
+
+window.compose.onBeforeSend.addListener(() => {
+    console.log('wacken')
+})
+
+console.log('hehe?')
+function drugs() {
+
+    console.log('wasn jetzt?');
+
+
+    console.log('warum ned?!');
+}
+
+drugs();
